b02e2d5443d3f578fe0692659133da4d19ccfdf1
karGuimaraes <kagm2047@gmail.com>
2024-03-21 17:57:59 -0300

    feat: permissions

---
+++ src/main/java/com/authorizer/authorizer/role/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.authorizer.authorizer.role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long>{
+    
+}
---
+++ src/main/java/com/authorizer/authorizer/role/RoleSeeder.java
@@ -0,0 +1,45 @@
+package com.authorizer.authorizer.role;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class RoleSeeder implements ApplicationRunner {
+    private final RoleRepository roleRepository;
+    private static final Logger log = LoggerFactory.getLogger(RoleSeeder.class);
+
+    @Autowired
+    public RoleSeeder(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public void run(ApplicationArguments args) {
+        if(args.getOptionValues("seeder") != null){
+            List<String> seeders = Arrays.asList(args.getOptionValues("seeder").get(0).split(","));
+            if (seeders.contains("role")) {
+                seedRoles();
+                log.info("Role seeder executed successfully");
+            }
+        } else {
+            log.info("Role seeder skipped");
+        }
+    }
+
+    private void seedRoles() {
+        List<String> roles = Arrays.asList("PRODUCT_READ", "PRODUCT_WRITE", "PRODUCT_DELETE", "PRODUCT_UPDATE");
+
+        for (String roleName : roles) {
+            Role role = new Role(null, roleName);
+            roleRepository.save(role);
+            log.info("Role seeded: {}", roleName);
+        }
+    }
+}
